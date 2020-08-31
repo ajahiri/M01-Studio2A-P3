92947cc9939a67bbcdf648ed7d04483967fe602a
younesDev00 <53510760+younesDev00@users.noreply.github.com>
2020-08-31 18:48:56 +1000

    vw to 100

--- imports/ui/pages/dashboard/dashboard.js
+++ imports/ui/pages/dashboard/dashboard.js
@@ -2,10 +2,11 @@ import './dashboard.html';
 import { Meteor } from 'meteor/meteor'
 // Only for login/signup showcase, should not have too much code for pages
 Template.App_dashboard.events({ 
-    'click #logoutButton': function(event, template) { 
-         Meteor.logout(function() { 
-            FlowRouter.go('/tutor-login');
-         });
+    'click #changeNameButton': function(event, template) { 
+        event.preventDefault();
+        event.stopPropagation();
+        console.log(Meteor.user());
+        Meteor.users.update({_id:Meteor.user()._id}, {$set:{"profile.name":$('#changeName')[0].value}})
     } 
 });
 
