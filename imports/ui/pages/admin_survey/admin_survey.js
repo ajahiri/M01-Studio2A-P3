16df45acfb6d0bbb3d77eab1cf695a6fd854204c
RaoufYounesBuzzy <68930552+RaoufYounesBuzzy@users.noreply.github.com>
2020-09-01 21:55:18 +1000

    UI Phone fixes

--- imports/ui/pages/admin_survey/admin_survey.js
+++ imports/ui/pages/admin_survey/admin_survey.js
@@ -1,10 +1,14 @@
 import './admin_survey.html';
 import { template } from 'lodash';
 
-
 Template.admin_survey.helpers({
     jack(){
         return "jack";
     }
     
-})
+});
+
+Template.admin_survey.onCreated(function(){
+    console.log('oncreated');
+    
+});
