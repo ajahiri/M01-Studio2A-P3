8a1ae27aad0de7c622329391e84824459e6b0f8b
Arian Jahiri <ajahiri.ait@gmail.com>
2020-09-27 21:18:13 +1000

    Added manual allocation/reallocating ability to editgroups

--- imports/api/projects/methods.js
+++ imports/api/projects/methods.js
@@ -34,4 +34,17 @@ Meteor.methods({
             owner: this.userId
         });
     },
+    updateGroupsArray(projectPayload) {
+        // Payload is the whole project object including the groups array inside it
+        // Check owner first
+        if (!this.userId) throw new Meteor.Error(403, 'No permission, must be logged in.');
+        const project = Projects.find({_id: projectPayload._id});
+        if (project.fetch().length < 1) throw new Meteor.Error(404, 'Could not find project with that ID');
+        if (project.fetch()[0].owner !== this.userId) throw new Meteor.Error(403, 'No permission, you are not the owner of this project.');
+        // End checking, do update now
+
+        const newGroups = projectPayload.groups;
+        console.log('NEW GROUPS ARRAY', newGroups);
+        Projects.update({_id: projectPayload._id}, {$set: {groups: newGroups}});
+    }
 })
--- imports/api/projects/server/publications.js
+++ imports/api/projects/server/publications.js
@@ -4,6 +4,7 @@ import { Projects } from '../projects';
 Meteor.publish('projectByID', function(projectID) {
     try {
         const result = Projects.find({_id: projectID});
+        if (result.fetch().length < 1) throw new Meteor.Error(404, 'Could not find project');
         return result;
     } catch (error) {
         throw new Meteor.Error(404, 'Could not find project');
