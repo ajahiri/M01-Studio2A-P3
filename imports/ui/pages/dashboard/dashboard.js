f74b6d4d05608c57fefc21f4a0d44bce523748b0
Arian Jahiri <ajahiri.AIT@gmail.com>
2020-10-16 18:56:00 +1100

    Complete change password reimplementation

--- imports/ui/pages/dashboard/dashboard.js
+++ imports/ui/pages/dashboard/dashboard.js
@@ -1,5 +1,6 @@
 import './dashboard.html';
-import { Meteor } from 'meteor/meteor'
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 // Only for login/signup showcase, should not have too much code for pages
 Template.App_dashboard.events({ 
     'click #changeNameButton': function(event, template) { 
@@ -7,7 +8,26 @@ Template.App_dashboard.events({
         event.stopPropagation();
         console.log(Meteor.user());
         Meteor.users.update({_id:Meteor.user()._id}, {$set:{"profile.name":$('#changeName')[0].value}})
-    } 
+    },
+    'click #changePasswordButton': function(event, template) {
+        event.preventDefault();
+        const oldPassword = $('#originalPassword').val();
+        const newPassword = $('#newPassword').val();
+        const confirmPassword = $('#confirmPassword').val();
+
+        if (newPassword !== confirmPassword) {
+            swal("Error!", "Confirm password must match new password.", "error");
+            return;
+        };
+
+        Accounts.changePassword(oldPassword, newPassword, function(error) {
+            if (!error) {
+                swal("Sucess!", "Your password was successfully changed.", "success");
+            } else {
+                swal("Error changing password!", error.reason, "error");
+            }
+        });
+    }
 });
 
 Template.App_dashboard.helpers({
